a5211c3e259902714af620480e230265b23ce619
Akash Nandi <akashnandi0@gmail.com>
2021-03-10 17:52:17 +0530

    funcbaseauthenticate

---
+++ gs12/api/views.py
@@ -0,0 +1,56 @@
+# FUNCTION-BASED AUTHENTICATION AND PERMISSION
+
+from django.shortcuts import render
+from rest_framework.decorators import api_view,authentication_classes,permission_classes
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializers
+from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def student_api(request,pk=None):
+    if request.method == 'GET':
+        id = pk
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializers(stu)
+            return Response(serializer.data)
+        stu = Student.objects.all()
+        serializer = StudentSerializers(stu,many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = StudentSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Data Created'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PUT':
+        id = pk
+        stu = Student.objects.get(pk=id)
+        serializer = StudentSerializers(stu, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Complete Data Updated'})
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PATCH':
+        id = pk
+        stu = Student.objects.get(pk=id)
+        serializer = StudentSerializers(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Partial Data Updated'})
+        return Response(serializer.errors)    
+    
+    if request.method == 'DELETE':
+        id = pk
+        stu = Student.objects.get(pk=id)
+        stu.delete()
+        return Response({'msg':'Data Deleted'})
